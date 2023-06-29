7c51d5c65930a05913d9ba37da62260bfce2cfc7
Ps1789 <137678114+Ps1789@users.noreply.github.com>
2023-06-29 13:50:01 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head> <title> button</title></head>
+<body>
+<form method="get" action="indexAction.php">
+<table border="1">
+<tr><td> id </td><td><input type="text" name="id"/></td></tr>
+<tr><td> username </td><td><input type="text" name="txtSno"/></td></tr>
+<tr><td> password</td><td><input type="text" name="txtSname"/></td></tr>
+<tr><td> usertype</td><td><input type="text" name="txtCourse"/></td></tr>
+<tr><td colspan="2">
+<input type="submit" name="btnsubmit" value="INSERT"/>
+<input type="submit" name="btnsubmit" value="UPDATE"/>
+<input type="submit" name="btnsubmit" value="DELETE"/>
+</td></tr>
+</table>
+
+</form>
+</body>
+</html>
+<?php
+?>
